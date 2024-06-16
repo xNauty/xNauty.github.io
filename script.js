8f5fb8adca5ee0057001b4b4af55a30abd1d5008
Karol <52219849+xNauty@users.noreply.github.com>
2024-06-16 23:30:43 +0200

    Update script.js
    Add new animation to "interests-section"

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
+function toggleMenu() {
+    const menuItems = document.querySelector('.menu-items');
+    menuItems.classList.toggle('show');
+}
+
+
 function copyEmailToClipboard(event) {
     event.preventDefault();
     navigator.clipboard.writeText('karolsys@gmail.com').then(() => {
-        // Wyświetl okno dialogowe zamiast tooltipu
         alert("+----------------------------------+\n" +
-      " |                                                   |\n" +
-      " |     Skopiowano adres email!      |\n" +
-      " |     ▶ karolsys@gmail.com ◀     |\n" +
-      " |                                                   |\n" +
-      "+----------------------------------+\n");
+            " |                                                   |\n" +
+            " |     Skopiowano adres email!      |\n" +
+            " |     ▶ karolsys@gmail.com ◀     |\n" +
+            " |                                                   |\n" +
+            "+----------------------------------+\n");
 
     }).catch(err => {
         console.error('Nie udało się skopiować emaila:', err);
     });
 }
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
-    // Funkcja do losowania koloru z określonymi ograniczeniami
-    function getRandomColorComponent() {
-        var value = Math.floor(Math.random() * 256); // Losowanie wartości od 0 do 255
-        if (value <= 50) {
-            return 0; // Wartości z przedziału 0-50 są zamieniane na 0
-        } else if (value <= 100) {
-            return 100; // Wartości z przedziału 51-100 są zamieniane na 100
-        }
-        return value; // Pozostałe wartości pozostają bez zmian
+    function updateTime() {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        const clock = `${hours}:${minutes}:${seconds}`;
+        
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const date = now.toLocaleDateString('pl-PL', options);
+
+        document.getElementById('clock').textContent = clock;
+        document.getElementById('date').textContent = date;
     }
-  
-    // Funkcja do tworzenia łańcucha koloru RGB
+
+    setInterval(updateTime, 1000);
+    updateTime();
+
+    function getRandomColorComponent() {
+        var value = Math.floor(Math.random() * 256);
+        if (value <= 50) {
+            return 0;
+        } else if (value <= 100) {
+            return 100;
+        }
+        return value;
+    }
+
     function getRandomRgbColor() {
         var r = getRandomColorComponent();
         var g = getRandomColorComponent();
@@ -38,23 +56,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function getRandomShade(){
         var value = Math.floor(Math.random() * (125 - 75 + 1) + 75);
-        return value
+        return value;
     }
-  
-    // Pobierz wszystkie elementy z klasą .skill
+
     var skills = document.querySelectorAll('.skill');
-  
-    // Ustaw nasłuchiwacz zdarzeń dla każdego kafelka umiejętności
+
     skills.forEach(function(skill) {
         skill.addEventListener('mouseenter', function() {
-            // Losuj kolor cienia
             var color = getRandomRgbColor();
             var shade = getRandomShade();
             skill.style.boxShadow = `0px 0px ${shade}px ${color}`;
         });
         skill.addEventListener('mouseleave', function() {
-            // Usuń cień po opuszczeniu kafelka
             skill.style.boxShadow = 'none';
+        });
+    });
+
+    const interests = document.querySelectorAll('.interest');
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 200);
+        const g = Math.floor(Math.random() * 200);
+        const b = Math.floor(Math.random() * 200);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    interests.forEach(interest => {
+        interest.addEventListener('mouseover', function () {
+            const randomColor = getRandomColor();
+            this.style.backgroundColor = randomColor;
         });
     });
 });
